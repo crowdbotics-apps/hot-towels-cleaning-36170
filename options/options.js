e96b15d84aba649aa394c2a9a6610d818d8ad84f
Crowdbotics <team@crowdbotics.com>
2022-07-13 02:18:11 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "hot_towels_cleaning_36170",
+  url: "https://hot_towels_cleaning_36170.botics.co",
+  api: "https://hot_towels_cleaning_36170.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
